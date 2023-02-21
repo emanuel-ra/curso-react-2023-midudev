32310472e389b1edb6d806db67f40ecd3c63b614
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-02-21 17:33:21 -0600

    03 mouse follower

--- twitterCard/src/App.jsx
+++ twitterCard/src/App.jsx
@@ -1,37 +1,34 @@
-import { useState } from "react";
-import TwitterFollowCard from "./TwitterFollowCard";
+import TwitterFollowCard from './TwitterFollowCard'
 
 const users = [
-    {
-        userName:'midudev' ,
-        name:'Miguel Àngel Duran' ,
-        isFollowing:true 
-    } ,
-    {
-        userName:'emanuel-ra' ,
-        name:'Emanuel Ramirez Abarca' ,
-        isFollowing:false
-    }
+  {
+    userName: 'midudev',
+    name: 'Miguel Àngel Duran',
+    isFollowing: true
+  },
+  {
+    userName: 'emanuel-ra',
+    name: 'Emanuel Ramirez Abarca',
+    isFollowing: false
+  }
 ]
 
 export function App () {
-    
-    return (
-        <section className="app">
-            {
-                users.map( (user,index) => {
-                    const {userName,name,isFollowing} = user
-                    return (
-                            <TwitterFollowCard  
-                                key={`tw-followCard-${index}`}                
-                                userName={userName} initialIsFollowing={isFollowing}
-                                >
-                                {name}
-                            </TwitterFollowCard>
-                        )
+  return (
+    <section className='app'>
+      {
+                users.map((user, index) => {
+                  const { userName, name, isFollowing } = user
+                  return (
+                    <TwitterFollowCard
+                      key={`tw-followCard-${index}`}
+                      userName={userName} initialIsFollowing={isFollowing}
+                    >
+                      {name}
+                    </TwitterFollowCard>
+                  )
                 })
             }
-        </section>
-    )
+    </section>
+  )
 }
-
