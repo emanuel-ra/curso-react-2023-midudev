a2c7c964ae3d48ceb60464797bbf2a16660123d9
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-02-21 18:22:55 -0600

    third project finished

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,8 +1,51 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const FollowMouse = () => {
+  const [enable, setEnable] = useState()
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enable) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    // cleanup
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias
+    return () => { window.removeEventListener('pointermove', handleMove) }
+  }, [enable])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => { setEnable(!enable) }}>{enable ? 'Desactivar' : 'Activar'}</button>
+    </>
+  )
+}
 
 function App () {
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
