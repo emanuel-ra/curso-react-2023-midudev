80c4741ffa168ecfe9fa2167b37ac47652dc4580
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-02-24 18:27:33 -0600

    Image of cat

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -1,31 +1,46 @@
 import { useEffect, useState } from 'react'
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/hello'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/'
+//const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/hello'
 
 export function App () {
   const [fact, setFact] = useState()
-
+  const [imageUrl, setImageUrl] = useState()
+  const [factErrror, setFactError] = useState()
+  
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
+      .then(res => {
+        if(!res.ok){
+          setFactError('No se ha podido recuperar la cita')
+        }
+        return res.json()
+      })
       .then(data => {
         const { fact } = data
         setFact(fact)
-
-        const threeFirstWord = fact.split(' ', 3)
-        fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            console.log(response)
-          })
       })
   }, [])
 
+  useEffect(() => {
+    if (!fact) return
+
+    const threeFirstWord = fact.split(' ', 3).join()
+    fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
+
   return (
-    <main>
+    <main className=''>
       <h1>App de Gatitos</h1>
-      {fact && <p>{fact}</p>}
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first words for ${fact} `} />}
     </main>
   )
 }
