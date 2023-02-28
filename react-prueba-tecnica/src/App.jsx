0e590854b0453f99dc4dfb4ec81a68e570b64942
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-02-27 18:26:02 -0600

    ....

--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -3,26 +3,27 @@ import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/'
-//const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/hello'
+// const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/hello'
 
 export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
   const [factErrror, setFactError] = useState()
-  
-  useEffect(() => {
+
+  const getRandomFact = () => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => {
-        if(!res.ok){
-          setFactError('No se ha podido recuperar la cita')
-        }
+      // TODO: HANDLE ERROR IF !RES.OK
+        if (!res.ok) { setFactError('No se ha podido recuperar la cita') }
         return res.json()
       })
       .then(data => {
         const { fact } = data
         setFact(fact)
       })
-  }, [])
+  }
+
+  useEffect(getRandomFact, [])
 
   useEffect(() => {
     if (!fact) return
@@ -36,11 +37,16 @@ export function App () {
       })
   }, [fact])
 
+  const handleClick = () => {
+    getRandomFact()
+  }
+
   return (
     <main className=''>
       <h1>App de Gatitos</h1>
-        {fact && <p>{fact}</p>}
-        {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first words for ${fact} `} />}
+      <button onClick={handleClick}>Get new fact</button>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first words for ${fact} `} />}
     </main>
   )
 }
