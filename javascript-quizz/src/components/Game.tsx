c9e77b2bfdabe16c16ed2ece0dfab51cbb2c3149
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-01-11 16:57:02 -0600

    project finish

--- javascript-quizz/src/components/Game.tsx
+++ javascript-quizz/src/components/Game.tsx
@@ -1,8 +1,30 @@
 import { Card, IconButton, List, ListItem, ListItemButton, ListItemText, Stack, Typography } from "@mui/material"
+import { ArrowBackIosNew, ArrowForwardIos  } from "@mui/icons-material"
 import { useQuestionStore } from "../store/questions"
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import { type Question as QuestionType } from "../types"
+import { Footer } from "./Footer";
+
+const getBackgroundColor = (info:QuestionType,index:number)=>{
+
+    const { userSelectedAnswer, correctAnswer } = info
+    
+    // user didn't select any 
+    if(userSelectedAnswer == null) return 'transparent'
+
+    //if select but the answer is wrong 
+    if(index !== correctAnswer && index !== userSelectedAnswer) return 'transparent'
+
+    // if is the correct answer
+    if(index === correctAnswer) return 'green'
+
+    // if this is the selected option, but is wrong
+    if(index === userSelectedAnswer) return 'red'
+
+    // if is nothing         
+    return 'transparent'
+}
 
 const Question = ({info}:{info:QuestionType}) => {
     const selectAnswer = useQuestionStore(state => state.selectAnswer)
@@ -11,6 +33,9 @@ const Question = ({info}:{info:QuestionType}) => {
         selectAnswer(info.id, answerIndex)
 
     }
+
+    
+    
     return (
         <Card variant="outlined" sx={{bgcolor:'#222',textAlign:'left'}}>
             <Typography variant="h5">
@@ -23,7 +48,13 @@ const Question = ({info}:{info:QuestionType}) => {
             <List sx={{bgcolor:'#333'}} disablePadding >
                 {info.answers.map((answer,index)=>(
                     <ListItem key={index} disablePadding divider >
-                        <ListItemButton onClick={createHandleClick(index)}>
+                        <ListItemButton 
+                            disabled={info.userSelectedAnswer!=null}
+                            onClick={createHandleClick(index)}
+                            sx={{
+                                backgroundColor:getBackgroundColor(info,index)
+                            }}
+                        >
                             <ListItemText primary={answer} sx={{textAlign:'center'}} />
                         </ListItemButton>
                     </ListItem>
@@ -36,11 +67,25 @@ const Question = ({info}:{info:QuestionType}) => {
 export const Game = () => {
     const questions = useQuestionStore(state => state.questions)
     const currentQuestion = useQuestionStore(state => state.currentQuestion)
+    const goNextQuestion = useQuestionStore(state => state.goNextQuestion)
+    const goPreviousQuestion = useQuestionStore(state => state.goPreviousQuestion)
 
     const questionInfo = questions[currentQuestion]
     return(
         <>
-            <Question info={questionInfo} />            
+            <Stack direction={`row`} gap={2} alignItems={`center`} justifyContent={`center`}>
+                <IconButton onClick={goPreviousQuestion} disabled={currentQuestion===0} >
+                    <ArrowBackIosNew />
+                </IconButton>
+
+                {currentQuestion+1} / {questions.length}
+
+                <IconButton onClick={goNextQuestion} disabled={currentQuestion>=questions.length-1}>
+                    <ArrowForwardIos />
+                </IconButton>
+            </Stack>
+            <Question info={questionInfo} />       
+            <Footer />                 
         </>
     )
 }
--- javascript-quizz/src/store/questions.ts
+++ javascript-quizz/src/store/questions.ts
@@ -1,42 +1,82 @@
 import { create } from "zustand";
 import { type Question } from "../types";
+import confetti from "canvas-confetti";
+import { persist, devtools } from "zustand/middleware";
+import { getAllQuestions } from "../services/questions";
 
 interface State {
   questions: Question[];
   currentQuestion: number;
   fetchQuestion: (limit: number) => void;
   selectAnswer: (question: number, answerIndex: number) => void;
+  goNextQuestion: () => void;
+  goPreviousQuestion: () => void;
+  reset: () => void;
 }
 
-export const useQuestionStore = create<State>((set, get) => {
-  return {
-    questions: [],
-    currentQuestion: 0,
-    fetchQuestion: async (limit: number) => {
-      const res = await fetch("http://localhost:5173/data.json");
-      const json = await res.json();
+export const useQuestionStore = create<State>()(
+  devtools(
+    persist(
+      (set, get) => {
+        return {
+          questions: [],
+          currentQuestion: 0,
+          fetchQuestion: async (limit: number) => {
+            const json = await getAllQuestions(limit);
+            const questions = json
+              .sort(() => Math.random() - 0.5)
+              .slice(0, limit);
+            set({ questions });
+          },
+          selectAnswer: (questionId: number, answerIndex: number) => {
+            const { questions } = get();
+            // clone the state
+            const newQuestions = structuredClone(questions);
+            // find the question index
+            const questionIndex = newQuestions.findIndex(
+              (q) => q.id === questionId
+            );
+            // get the question
+            const questionInfo = newQuestions[questionIndex];
+            // evaluate if is correct
+            const isCorrectUserAnswer =
+              questionInfo.correctAnswer === answerIndex;
 
-      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
-      set({ questions });
-    },
-    selectAnswer: (questionId: number, answerIndex: number) => {
-      const { questions } = get();
-      // clone the state
-      const newQuestions = structuredClone(questions);
-      // find the question index
-      const questionIndex = newQuestions.findIndex((q) => q.id === questionId);
-      // get the question
-      const questionInfo = newQuestions[questionIndex];
-      // evaluate if is correct
-      const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex;
-      // update the state
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrectUserAnswer,
-        userSelectedAnswer: answerIndex,
-      };
-      // update the state
-      set({ questions: newQuestions });
-    },
-  };
-});
+            if (isCorrectUserAnswer) confetti();
+
+            // update the state
+            newQuestions[questionIndex] = {
+              ...questionInfo,
+              isCorrectUserAnswer,
+              userSelectedAnswer: answerIndex,
+            };
+            // update the state
+            set({ questions: newQuestions });
+          },
+          goNextQuestion: () => {
+            const { currentQuestion, questions } = get();
+            const nextQuestion = currentQuestion + 1;
+
+            if (nextQuestion < questions.length) {
+              set({ currentQuestion: nextQuestion });
+            }
+          },
+          goPreviousQuestion: () => {
+            const { currentQuestion, questions } = get();
+            const previousQuestion = currentQuestion - 1;
+
+            if (previousQuestion >= 0) {
+              set({ currentQuestion: previousQuestion });
+            }
+          },
+          reset: () => {
+            set({ currentQuestion: 0, questions: [] });
+          },
+        };
+      },
+      {
+        name: "questions",
+      }
+    )
+  )
+);
